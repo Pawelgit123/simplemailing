45027b27d6a14219a49dbffef14f3b009171c0df
PG <aakilae@gmail.com>
2023-02-08 19:22:30 +0100

    mail -its alive!

--- src/main/java/com/exercise/simplemailing/emailThree/EmailService.java
+++ src/main/java/com/exercise/simplemailing/emailThree/EmailService.java
@@ -1,19 +1,23 @@
 package com.exercise.simplemailing.emailThree;
 
 
+import com.exercise.simplemailing.userWithMail.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
-
+    private UserWithMailServiceSearch userWithMailServiceSearch;
 
 
     public void sendEmail(Email email) {
@@ -26,7 +30,21 @@ public class EmailService {
             mailMessage.setSubject(email.getTitle());
 
             javaMailSender.send(mailMessage);
+    }
 
+    public void sendEmailToAll(String subject, String text){
 
+        UserWithMailDTOListed allUserWithMail = userWithMailServiceSearch.getAllUserWithMail();
+        Set<UserWithMailDTO> userWithMailDTOSet = allUserWithMail.getUserWithMailDTOSet();
+
+        for (UserWithMailDTO user: userWithMailDTOSet) {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom("testerpgtester@gmail.com");
+            mailMessage.setTo(user.getEmail());
+            mailMessage.setText(text);
+            mailMessage.setSubject(subject);
+
+            javaMailSender.send(mailMessage);
+        }
     }
 }
